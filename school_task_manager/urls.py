4d02113e11f008bd76109d3c27747375dd56d47c
Sara <sararossmay2000@gmail.com>
2025-10-23 02:32:38 +0300

    Add redirect api/ url to api/tasks/

--- school_task_manager/urls.py
+++ school_task_manager/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_root_redirect),
     path('api/users/', include('users.urls')),
     path('api/tasks/', include('tasks.urls')),
 
